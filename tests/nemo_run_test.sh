70e0a1cef28afa23b49958f75b6139cff8bf1926
Martin Schreiber <M.Schreiber@exeter.ac.uk>
2016-05-31 19:50:35 +0100

    added variable problem size to nemo test script

--- tests/nemo_run_test.sh
+++ tests/nemo_run_test.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-export JPI=150
-export JPJ=100
-export JPK=72
 
-#
-# Use papi_avail command to check available performance counters
-# These are part of papi-tools package
-#
-export NUM_COUNTERS=2
-export LIST_COUNTERS=FP_COMP_OPS_EXE,PAPI_L3_TCM
+for i in 1 2 4 8 16 32 64 128; do
+	export JPI=$i
+	export JPJ=$i
+	export JPK=72
+	export LD_LIBRARY_PATH=../build:$LD_LIBRARY_PATH
 
-export NUM_COUNTERS=1
-export LIST_COUNTERS=PAPI_L3_TCM
+	#
+	# Use papi_avail command to check available performance counters
+	# These are part of papi-tools package
+	#
+	export LIST_COUNTERS=FP_COMP_OPS_EXE,PAPI_L3_TCM
+	export LIST_COUNTERS=PAPI_L3_TCM
 
-taskset -c 0 ./nemo_test
+	echo "***********************************"
+	echo "SIZE: $i"
+	echo "***********************************"
+	taskset -c 0 ./nemo_test
+	echo "***********************************"
+done
